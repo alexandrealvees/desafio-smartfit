5019af02fa126abd2c07b9b188564ff0c9e17069
Alexandre Alves <alexandrealveees@outlook.com>
2025-01-15 23:47:37 -0300

    feat: footer component

---
+++ src/app/components/legend/legend.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-legend',
+  templateUrl: './legend.component.html',
+  standalone: true,
+  styleUrls: ['./legend.component.scss']
+})
+export class LegendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
